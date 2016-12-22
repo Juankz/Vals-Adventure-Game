fff6ed973afea16e42e46771769ed68d1671d827
Juankz <juanstsf@gmail.com>
2016-12-22 01:20:14 -0500

    New tiled sets with nature elements and customized elements

---
+++ android/assets/adventure_maps/springTrees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="springTrees" tilewidth="70" tileheight="70" tilecount="288" columns="12">
+ <image source="resources/springTrees.png" width="840" height="1680"/>
+</tileset>
