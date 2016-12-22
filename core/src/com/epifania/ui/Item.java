9781e549629cba73d934a9a2bbdf6d3213190f3d
Juankz <juanstsf@gmail.com>
2016-12-22 01:47:47 -0500

    Fixed display item images bigger than 70x70

--- core/src/com/epifania/ui/Item.java
+++ core/src/com/epifania/ui/Item.java
@@ -1,5 +1,6 @@
 package com.epifania.ui;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Container;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -26,13 +27,30 @@ public class Item extends Container<Image> {
     }
 
     public Item(Skin skin, TextureRegion region){
-        super(new Image(region));
+        super();
         this.skin = skin;
         init();
+        super.setActor(new Image(getCuttenRegion(region)));
+    }
+
+    private TextureRegion getCuttenRegion(TextureRegion region) {
+        int x = 0;
+        int y = 0;
+        int w = region.getRegionWidth();
+        int h = region.getRegionHeight();
+        //TODO revisar estas dimensiones
+        if(w>70){
+            w=70;
+        }
+        if(h>70){
+            h=70;
+        }
+
+        return new TextureRegion(region,x,y,70,70);
     }
 
     private void init(){
-        float size = 70;
+        float size = 80;
         prefHeight(size);
         prefWidth(size);
         setTransform(true);
