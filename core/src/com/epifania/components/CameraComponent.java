5b903d3f8878ed5b84480b5b3c6a3fd646973a47
Juankz <juanstsf@gmail.com>
2017-01-06 11:25:06 -0500

    Modified camera to give more visible space under Val feet

--- core/src/com/epifania/components/CameraComponent.java
+++ core/src/com/epifania/components/CameraComponent.java
@@ -7,9 +7,9 @@ import com.badlogic.gdx.math.Rectangle;
 
 public class CameraComponent implements Component {
 
-	public static final float DISTANCEX = 2f;
-	public static final float DISTANCEYU = -2f;
-	public static final float DISTANCEYD = 3f;
+	public static final float DISTANCEX = 2f; //Distance from the center at which the camera will move on the horizontal axis
+	public static final float DISTANCEYU = -1.7f; //Distance from the center at which the camera will move up
+	public static final float DISTANCEYD = 1.7f; //Distance from the center at which the camera will move down
 	public OrthographicCamera camera;
 	public final Rectangle bounds = new Rectangle();
 	public Entity target;
