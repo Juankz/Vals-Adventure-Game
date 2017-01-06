c7dddce42be3939c7c2ea4936dfb8883499ffa0f
Juankz <juanstsf@gmail.com>
2017-01-06 12:01:07 -0500

    Add new levels and modify script

---
+++ android/assets/adventure_maps/naval.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="naval" tilewidth="70" tileheight="70" spacing="2" margin="1" tilecount="392" columns="28">
+ <image source="resources/naval.png" width="2048" height="1024"/>
+</tileset>
