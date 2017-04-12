0847e760a87c4dd7d7ab3eae3d5f8ffadb27d962
juan <ibifuhur.56>
2017-04-12 18:56:11 -0500

    Update levels

---
+++ android/assets/adventure_maps/rune.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="rune" tilewidth="54" tileheight="60" spacing="2" margin="1" tilecount="9" columns="9">
+ <image source="resources/rune.png" width="512" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="key" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="key" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="key" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="key" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="key" type="int" value="5"/>
+  </properties>
+ </tile>
+</tileset>
