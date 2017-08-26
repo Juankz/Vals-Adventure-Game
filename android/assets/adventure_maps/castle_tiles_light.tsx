5744f036bfc861618f72aefb1bc9a21b621cac1a
juankz <juanvelandia@protonmail.com>
2017-08-26 09:14:54 -0500

    commited missed files

---
+++ android/assets/adventure_maps/castle_tiles_light.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="castle_tiles_light" tilewidth="70" tileheight="70" spacing="2" margin="1" tilecount="42" columns="14">
+ <image source="resources/castle_tiles_light.png" width="1024" height="256"/>
+</tileset>
